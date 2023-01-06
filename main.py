edbf0e188f4c9e60a1626b283840153b63363389
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-06 14:42:09 -0300

    WASD movement (down sprite bug)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, KEYDOWN, MOUSEBUTTONDOWN
+from pygame.locals import K_w, K_s, K_a, K_d, KEYDOWN, MOUSEBUTTONDOWN
 from spider import Spider
 # 800x600 -> 50x50px sprites -> 10x10 board = 500x500px board
 game_screen = pygame.display.set_mode([800, 600])
@@ -16,8 +16,14 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
             elif event.type == KEYDOWN:
-                if event.key == K_UP:
-                    pass
+                if event.key == K_w:
+                    game_spider.update("up")
+                elif event.key == K_s:
+                    game_spider.update("down")
+                elif event.key == K_a:
+                    game_spider.update("left")
+                elif event.key == K_d:
+                    game_spider.update("right")
         
         game_screen.fill((0,0,0)) # placeholder bg
         game_spider.show(game_screen)
--- spider.py
+++ spider.py
@@ -15,13 +15,37 @@ class Spider (pygame.sprite.Sprite):
 
         self.direction = "down"
 
-        self.image = pygame.image.load(SPRITES["down"]).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (SIZE, SIZE))
+        self.sprite = pygame.image.load(SPRITES["down"]).convert_alpha()
+        self.image = pygame.transform.scale(self.sprite, (SIZE, SIZE))
         self.rect = pygame.Rect(self.x, self.y, SIZE, SIZE)
 
     def show (self, screen: pygame.Surface) -> None:
         screen.blit(self.image, self.rect)
 
-    def refresh (self, direction: str) -> None:
+    def update (self, direction: str) -> None:
         if direction == "up":
-            self.image = pygame.transform.rotate(self.image, 180)
+            self.y -= SIZE
+            if direction != self.direction:
+                self.image = pygame.transform.rotate(self.sprite, 180)
+            else: self.direction = direction
+        
+        elif direction == "down":
+            self.y += SIZE
+            if direction != self.direction:
+                self.image = pygame.image.load(SPRITES["down"]).convert_alpha()
+            else: self.direction = direction
+
+        elif direction == "left":
+            self.x -= SIZE
+            if direction != self.direction:
+                self.image = pygame.transform.rotate(self.sprite, 270)
+            else: self.direction = direction
+
+        elif direction == "right":
+            self.x += SIZE
+            if direction != self.direction:
+                self.image = pygame.transform.rotate(self.sprite, 90)
+            else: self.direction = direction
+        
+        self.image = pygame.transform.scale(self.image, (SIZE, SIZE))
+        self.rect.left, self.rect.top = self.x, self.y
